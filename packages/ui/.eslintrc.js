b819b52bace49991167979a548070e251489cc88
siandrev <andreev.sergey.i@yandex.ru>
2022-10-17 12:38:36 +0400

    configs refactored

---
+++ packages/ui/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    extends: ["../../.eslintrc.js", "plugin:solid/typescript"],
+    parserOptions: {
+        "sourceType": "module",
+        "ecmaVersion": 2020,
+        ecmaFeatures: {
+            jsx: true
+        }
+    },
+    plugins: ["solid"],
+    rules: {
+        "solid/components-return-once": "error"
+    }
+};
