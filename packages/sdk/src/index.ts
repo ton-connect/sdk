9446755e6349f64f5d9da131865de382cdd41455
siandrev <andreev.sergey.i@yandex.ru>
2023-01-17 12:52:08 +0400

    fix(sdk): tonProof models rexeproted

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -4,5 +4,12 @@ export * from './errors';
 export { IStorage } from './storage/models/storage.interface';
 export { TonConnect as default } from './ton-connect';
 export { ITonConnect } from './ton-connect.interface';
-export { CHAIN, DeviceInfo } from '@tonconnect/protocol';
+export {
+    CHAIN,
+    DeviceInfo,
+    Feature,
+    TonProofItemReply,
+    TonProofItemReplySuccess,
+    TonProofItemReplyError
+} from '@tonconnect/protocol';
 export { toUserFriendlyAddress } from './utils/address';
