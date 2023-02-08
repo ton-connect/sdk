0254ad0376a33e44c19e33df28be022b7b2f14e5
siandrev <andreev.sergey.i@yandex.ru>
2023-02-08 14:38:10 +0400

    fix(ui-react): useTonWallet hook refactored

--- packages/ui-react/src/hooks/useTonWallet.ts
+++ packages/ui-react/src/hooks/useTonWallet.ts
@@ -1,19 +1,20 @@
 import { useTonConnectUI } from './useTonConnectUI';
-import { Wallet, WalletInfo } from '@tonconnect/sdk';
+import { Wallet } from '@tonconnect/sdk';
 import { useEffect, useState } from 'react';
 import { isServerSide } from '../utils/web';
+import { WalletInfoWithOpenMethod } from '@tonconnect/ui';
 
 /**
  * Use it to get user's current ton wallet. If wallet is not connected hook will return null.
  */
-export function useTonWallet(): (Wallet & WalletInfo) | null {
+export function useTonWallet(): (Wallet & WalletInfoWithOpenMethod) | null {
     if (isServerSide()) {
         return null;
     }
 
     const [tonConnectUI] = useTonConnectUI();
-    const [wallet, setWallet] = useState<(Wallet & WalletInfo) | null>(
-        () => tonConnectUI.wallet && { ...tonConnectUI.wallet, ...tonConnectUI.walletInfo! }
+    const [wallet, setWallet] = useState<(Wallet & WalletInfoWithOpenMethod) | null>(
+        tonConnectUI.wallet
     );
 
     useEffect(
