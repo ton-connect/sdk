3bc5f689779807b8a78784484f48e849e83544f9
siandrev <andreev.sergey.i@yandex.ru>
2023-03-09 12:22:46 +0400

    fix(protocol): `Features` format changed

--- packages/protocol/src/models/feature.ts
+++ packages/protocol/src/models/feature.ts
@@ -1,1 +1,5 @@
-export type Feature = 'SendTransaction';
+export type Feature = SendTransactionFeatureDeprecated | SendTransactionFeature | SignDataFeature;
+
+export type SendTransactionFeatureDeprecated = 'SendTransaction';
+export type SendTransactionFeature = { name: 'SendTransaction'; maxMessages: number };
+export type SignDataFeature = { name: 'SignData' };
--- packages/protocol/src/models/index.ts
+++ packages/protocol/src/models/index.ts
@@ -2,5 +2,10 @@ export * from './app-message';
 export * from './wallet-message';
 export { RpcMethod } from './rpc-method';
 export { DeviceInfo } from './device-info';
-export { Feature } from './feature';
+export {
+    Feature,
+    SendTransactionFeatureDeprecated,
+    SendTransactionFeature,
+    SignDataFeature
+} from './feature';
 export { CHAIN } from './CHAIN';
