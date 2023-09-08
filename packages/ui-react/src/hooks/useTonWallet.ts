456f5ebdc061b35aa131c0f2a892dc49ebdcf2ba
siandrev <andreev.sergey.i@yandex.ru>
2023-09-08 13:48:20 +0200

    fix(ui-react): useTonWallet hook types changed BREAKING CHANGE: `useTonWallet` now returns `Wallet | (Wallet & WalletInfoWithOpenMethod) | null` instead of `(Wallet & WalletInfoWithOpenMethod) | null`

--- packages/ui-react/src/hooks/useTonWallet.ts
+++ packages/ui-react/src/hooks/useTonWallet.ts
@@ -5,9 +5,9 @@ import { WalletInfoWithOpenMethod, Wallet } from '@tonconnect/ui';
 /**
  * Use it to get user's current ton wallet. If wallet is not connected hook will return null.
  */
-export function useTonWallet(): (Wallet & WalletInfoWithOpenMethod) | null {
+export function useTonWallet(): Wallet | (Wallet & WalletInfoWithOpenMethod) | null {
     const [tonConnectUI] = useTonConnectUI();
-    const [wallet, setWallet] = useState<(Wallet & WalletInfoWithOpenMethod) | null>(
+    const [wallet, setWallet] = useState<Wallet | (Wallet & WalletInfoWithOpenMethod) | null>(
         tonConnectUI?.wallet || null
     );
 
