512678ff25ae877a7a2f608e8d8c06fda5dcbd21
thekiba <hr@thekiba.io>
2023-10-10 16:09:39 +0400

    fix(ui): resolve illegal constructor error in safari
    Closes #87

--- packages/ui/src/app/utils/web-api.ts
+++ packages/ui/src/app/utils/web-api.ts
@@ -81,9 +81,7 @@ export function fixMobileSafariActiveTransition(): void {
 }
 
 export function defineStylesRoot(): void {
-    customElements.define(globalStylesTag, class TcRootElement extends HTMLDivElement {}, {
-        extends: 'div'
-    });
+    customElements.define(globalStylesTag, class TcRootElement extends HTMLElement {});
 }
 
 export function preloadImages(images: string[]): void {
