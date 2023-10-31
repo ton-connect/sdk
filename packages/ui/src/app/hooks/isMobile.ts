a34ce5b33077bf77739a96006335fe7b605dd96f
thekiba <hr@thekiba.io>
2023-10-31 18:09:52 +0400

    fix(ui): correct detection of mobile devices for modal widget

--- packages/ui/src/app/hooks/isMobile.ts
+++ packages/ui/src/app/hooks/isMobile.ts
@@ -5,6 +5,9 @@ import { getWindow } from 'src/app/utils/web-api';
 const [isMobile, setIsMobile] = createSignal(isDevice('mobile'));
 
 if (getWindow()) {
+    // It's important to check the device type on page load because the value of window.innerWidth can change after the page has loaded
+    window.addEventListener('load', () => setIsMobile(isDevice('mobile')));
+
     window.addEventListener('resize', () => setIsMobile(isDevice('mobile')));
 }
 
