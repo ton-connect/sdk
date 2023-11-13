fe77e6c66b71051684ecb17fede591172c9d70d2
thekiba <hr@thekiba.io>
2023-11-14 03:40:05 +0400

    refactor(sdk): change return type of isTelegramUrl

--- packages/sdk/src/utils/url.ts
+++ packages/sdk/src/utils/url.ts
@@ -10,7 +10,11 @@ export function addPathToUrl(url: string, path: string): string {
     return removeUrlLastSlash(url) + '/' + path;
 }
 
-export function isTelegramUrl(link: string): boolean {
+export function isTelegramUrl(link: string | undefined): link is string {
+    if (!link) {
+        return false;
+    }
+
     const url = new URL(link);
     return url.protocol === 'tg:' || url.hostname === 't.me';
 }
