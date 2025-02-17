7b22880ee0a506608275a83d778ee808d52a9449
thekiba <hr@thekiba.io>
2025-02-18 01:09:15 +0400

    feat(protocol): add `extraCurrenciesSupported` flag to `SendTransaction` feature

--- packages/protocol/src/models/feature.ts
+++ packages/protocol/src/models/feature.ts
@@ -1,5 +1,9 @@
 export type Feature = SendTransactionFeatureDeprecated | SendTransactionFeature | SignDataFeature;
 
 export type SendTransactionFeatureDeprecated = 'SendTransaction';
-export type SendTransactionFeature = { name: 'SendTransaction'; maxMessages: number };
+export type SendTransactionFeature = {
+    name: 'SendTransaction';
+    maxMessages: number;
+    extraCurrenciesSupported?: boolean;
+};
 export type SignDataFeature = { name: 'SignData' };
