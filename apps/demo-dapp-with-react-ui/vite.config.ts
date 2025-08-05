ab5cafc91b7a45fd6e11bee40d720d4f58f25e7b
Tim <truecarry@gmail.com>
2025-08-05 13:55:27 +0300

    Disable docs push on test branch, disable GH_PAGES env for dapp

--- apps/demo-dapp-with-react-ui/vite.config.ts
+++ apps/demo-dapp-with-react-ui/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     outDir: 'docs'
   },
   // @ts-ignore
-  base: process.env.GH_PAGES ? '/demo-dapp-with-react-ui/' : './',
+  base: './',
   server: {
     fs: {
       allow: ['../sdk', './'],
