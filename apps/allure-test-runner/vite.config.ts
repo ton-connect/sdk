acf29bd4e06b51b2a91ddf3d146480305aab58f6
Alejandbel <bogdanov28c@gmail.com>
2025-09-04 23:12:29 +0300

    feat: remove unify for better errors

--- apps/allure-test-runner/vite.config.ts
+++ apps/allure-test-runner/vite.config.ts
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()]
+    plugins: [react()],
+    build: {
+        minify: false
+    }
 });
