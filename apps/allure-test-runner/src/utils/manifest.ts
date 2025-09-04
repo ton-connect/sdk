9bbddc50a51b0d152219f35cf8d09c8b8dbb9be5
Alejandbel <bogdanov28c@gmail.com>
2025-09-04 19:01:43 +0300

    feat: try change manifest

--- apps/allure-test-runner/src/utils/manifest.ts
+++ apps/allure-test-runner/src/utils/manifest.ts
@@ -1,7 +1,17 @@
 import { TonConnectUI } from '@tonconnect/ui';
 
 export function changeManifestUrl(tonConnectUI: TonConnectUI, newUrl: string | undefined) {
-    (
-        tonConnectUI.connector as unknown as { dappSettings: { manifestUrl: string | undefined } }
-    ).dappSettings.manifestUrl = newUrl;
+    const val = tonConnectUI as unknown as {
+        connector: { dappSettings: { manifestUrl: string | undefined } };
+    };
+
+    // TODO: not working
+    console.log(`Changing manifest url to ${newUrl}`, val.connector);
+    console.log(val.connector.dappSettings.manifestUrl);
+
+    val.connector.dappSettings.manifestUrl = newUrl;
+
+    console.log(val.connector.dappSettings.manifestUrl);
+    console.log(`Changed manifest url to ${newUrl}`, val.connector);
+    console.log(val.connector.dappSettings.manifestUrl);
 }
