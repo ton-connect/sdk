b03fdb9099f36e33225d35d676f32d7c294de651
Ilyar <ilyar.software@gmail.com>
2025-09-25 10:14:31 +0200

    feat: add mode local for demo dapp

--- apps/demo-dapp-with-react-ui/src/App.tsx
+++ apps/demo-dapp-with-react-ui/src/App.tsx
@@ -1,4 +1,5 @@
 import './App.scss';
+import { UIWallet } from '@tonconnect/ui';
 import { THEME, TonConnectUIProvider } from '@tonconnect/ui-react';
 import { Header } from './components/Header/Header';
 import { TxForm } from './components/TxForm/TxForm';
@@ -11,11 +12,43 @@ import { MerkleExample } from './components/MerkleExample/MerkleExample';
 import { FindTransactionDemo } from './components/FindTransactionDemo/FindTransactionDemo';
 import { TransferUsdt } from './components/TransferUsdt/TransferUsdt';
 
+const walletLocal: UIWallet = {
+    app_name: 'wallet_kit_demo_wallet_local',
+    name: 'WalletKitDemoWalletLocal',
+    image: 'https://ton.org/download/ton_symbol.png',
+    about_url: 'https://walletkit-demo-wallet.vercel.app',
+    universal_url: 'https://walletkit-demo-wallet.vercel.app/ton-connect',
+    bridge: [
+        {
+            type: 'js',
+            key: 'walletKitDemoWallet'
+        },
+        {
+            type: 'sse',
+            url: 'http://localhost:8081/bridge'
+        }
+    ],
+    platforms: ['chrome', 'safari', 'firefox', 'ios', 'android'],
+    features: [
+        {
+            name: 'SendTransaction',
+            maxMessages: 255,
+            extraCurrencySupported: true
+        },
+        {
+            name: 'SignData',
+            types: ['text', 'binary', 'cell']
+        }
+    ]
+};
+
 function App() {
+    const isLocalMode = new URLSearchParams(window.location.search).get('mode') === 'local';
     return (
         <TonConnectUIProvider
             manifestUrl="https://tonconnect-sdk-demo-dapp.vercel.app/tonconnect-manifest.json"
             uiPreferences={{ theme: THEME.DARK }}
+            walletsListConfiguration={isLocalMode ? walletLocal : {}}
         >
             <div className="app">
                 <Header />
