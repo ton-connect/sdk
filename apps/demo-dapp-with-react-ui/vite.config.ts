beb31b373e0d9db4b7d0bfd55a1ab0d0a439b74a
Tim <truecarry@gmail.com>
2025-10-21 21:38:29 +0300

    Dev/demo dapp router (#456)

--- apps/demo-dapp-with-react-ui/vite.config.ts
+++ apps/demo-dapp-with-react-ui/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
         sourcemap: true
     },
     // @ts-ignore
-    base: './',
+    base: '/',
     server: {
         fs: {
             allow: ['../sdk', './']
