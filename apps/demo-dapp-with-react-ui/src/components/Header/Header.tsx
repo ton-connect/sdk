59bb277dc38c0dfabbddc8a80c30fab44ae1a723
Alejandbel <bogdanov28c@gmail.com>
2025-12-11 11:45:15 +0300

    feature: reset wallet list

--- apps/demo-dapp-with-react-ui/src/components/Header/Header.tsx
+++ apps/demo-dapp-with-react-ui/src/components/Header/Header.tsx
@@ -1,6 +1,18 @@
-import { TonConnectButton } from '@tonconnect/ui-react';
+import { TonConnectButton, useTonConnectUI } from '@tonconnect/ui-react';
 import { NetworkPicker } from '../NetworkPicker/NetworkPicker';
 import './header.scss';
+import { TonConnectUI } from '@tonconnect/ui-react';
+
+function revalidateCache(tonConnect: TonConnectUI) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (tonConnect.connector as any).walletsList.walletsListDTOCache = null;
+}
+
+const ResetWalletList = () => {
+    const [tonConnect] = useTonConnectUI();
+
+    return <button onClick={() => revalidateCache(tonConnect)}>Reset wallet list</button>;
+};
 
 export const Header = () => {
     return (
@@ -8,6 +20,7 @@ export const Header = () => {
             <span>My App with React UI</span>
             <div className="header__controls">
                 <NetworkPicker />
+                <ResetWalletList />
                 <TonConnectButton />
             </div>
         </header>
