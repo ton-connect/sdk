0df37f755f5b7b20dd560ee23eb868470a06ab77
Yauheni Mileika <vip.carich@gmail.com>
2026-02-25 11:15:01 +0300

    feat: change requests properties

--- packages/sdk/src/models/methods/intents/sign-data-intent-request.ts
+++ packages/sdk/src/models/methods/intents/sign-data-intent-request.ts
@@ -2,6 +2,5 @@ import { ChainId, SignDataPayload } from '@tonconnect/protocol';
 
 export interface SignDataIntentRequest {
     network?: ChainId;
-    manifestUrl: string;
     payload: SignDataPayload;
 }
