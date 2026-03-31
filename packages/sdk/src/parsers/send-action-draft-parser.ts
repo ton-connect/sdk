853aa8309fdd36f76a56c6e09a078705cf0d85ff
Yauheni Mileika <vip.carich@gmail.com>
2026-03-31 17:09:24 +0300

    fix: error codes

--- packages/sdk/src/parsers/send-action-draft-parser.ts
+++ packages/sdk/src/parsers/send-action-draft-parser.ts
@@ -1,4 +1,4 @@
-import { AppRequest, SEND_TRANSACTION_ERROR_CODES } from '@tonconnect/protocol';
+import { AppRequest, SEND_ACTION_DRAFT_ERROR_CODES } from '@tonconnect/protocol';
 import { BadRequestError, TonConnectError, UnknownAppError, UserRejectsError } from 'src/errors';
 import { UnknownError } from 'src/errors/unknown.error';
 import { SendTransactionResponse, SignDataResponse, SignMessageResponse } from 'src/models/methods';
@@ -12,10 +12,10 @@ export type SendActionDraftResponse =
     | SignMessageResponse;
 
 const errorMap: Partial<Record<number, typeof TonConnectError>> = {
-    [SEND_TRANSACTION_ERROR_CODES.UNKNOWN_ERROR]: UnknownError,
-    [SEND_TRANSACTION_ERROR_CODES.USER_REJECTS_ERROR]: UserRejectsError,
-    [SEND_TRANSACTION_ERROR_CODES.BAD_REQUEST_ERROR]: BadRequestError,
-    [SEND_TRANSACTION_ERROR_CODES.UNKNOWN_APP_ERROR]: UnknownAppError
+    [SEND_ACTION_DRAFT_ERROR_CODES.UNKNOWN_ERROR]: UnknownError,
+    [SEND_ACTION_DRAFT_ERROR_CODES.USER_REJECTS_ERROR]: UserRejectsError,
+    [SEND_ACTION_DRAFT_ERROR_CODES.BAD_REQUEST_ERROR]: BadRequestError,
+    [SEND_ACTION_DRAFT_ERROR_CODES.UNKNOWN_APP_ERROR]: UnknownAppError
 };
 
 class SendActionDraftParser extends RpcParser<'actionDraft'> {
