fd64704d46faaaa51ef3a29f3b5fa9c0c7429847
Alejandbel <bogdanov28c@gmail.com>
2026-04-13 16:35:39 +0300

    feat: add retry to gasless

---
+++ apps/demo-dapp-with-react-ui/src/utils/retry.ts
@@ -0,0 +1,17 @@
+export async function retry<T>(
+    fn: () => Promise<T>,
+    options: { retries: number; delay: number }
+): Promise<T> {
+    let lastError: Error | undefined;
+    for (let i = 0; i < options.retries; i++) {
+        try {
+            return await fn();
+        } catch (e) {
+            if (e instanceof Error) {
+                lastError = e;
+            }
+            await new Promise(resolve => setTimeout(resolve, options.delay));
+        }
+    }
+    throw lastError;
+}
