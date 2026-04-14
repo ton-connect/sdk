671aa24e80f772f88e69476115e7c5828c1b3b4c
Alejandbel <bogdanov28c@gmail.com>
2026-04-14 17:28:47 +0300

    fix: add missing export

--- packages/sdk/src/models/wallet/index.ts
+++ packages/sdk/src/models/wallet/index.ts
@@ -24,5 +24,6 @@ export {
     RequiredFeatures,
     RequiredSendTransactionFeature,
     RequiredSignDataFeature,
-    RequiredSignMessageFeature
+    RequiredSignMessageFeature,
+    RequiredEmbeddedRequestFeature
 } from './require-feature';
